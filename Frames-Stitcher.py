ea1bb013275f8c0058d6fd6408162715b82338ca
jayrfs <rfsjay@gmail.com>
2021-08-14 00:55:06 +0530

    🏗️ Add: input and output paths

--- Frames-Stitcher.py
+++ Frames-Stitcher.py
@@ -1,22 +1,29 @@
 import cv2, os
 import numpy as np
 method = cv2.TM_SQDIFF_NORMED
-chapter = int(input(f"Choose chapter to stitch: {os.listdir('.//input//')} :"))
-prev_image = long_image = cv2.imread('input/%d_frames/0.jpg' % chapter)
-numberFiles = os.listdir(f"input/{chapter}_frames/")
 
-print(f"{len(numberFiles)} images detected in chapter {chapter}")
-
-#create input and output folders
+#create input and output folders if not present
 listFiles = os.listdir(".//")
 if "input" not in listFiles:
     os.mkdir("input")
 if "output" not in listFiles:
     os.mkdir("output")
 
+#add paths
+inputPath = ".//input//"
+outputPath = ".//output//"
+
+#get chapter
+chapter = int(input(f"Choose chapter to stitch: {os.listdir(inputPath)} :"))
+prev_image = long_image = cv2.imread(f"{inputPath}{chapter}_frames/0.jpg")
+numberFiles = os.listdir(f"{inputPath}{chapter}_frames/")
+
+#
+print(f"{len(numberFiles)} images detected in chapter {chapter}")
+
 count = 0
 for img_num in range(len(numberFiles)):
-    new_image = cv2.imread(f'input/{chapter}_frames/{img_num}.jpg')
+    new_image = cv2.imread(f'{inputPath}{chapter}_frames/{img_num}.jpg')
     prev_image_cropped = prev_image[-500:-1,:]
     # cv2.imshow('output', prev_image_cropped)
     # cv2.waitKey(0)
@@ -36,6 +43,6 @@ for img_num in range(len(numberFiles)):
 
     prev_image = new_image
     count+=1
-    print(count)
+    print(count,end=" ")
 
-cv2.imwrite(f'{chapter}_long.png', long_image)
+cv2.imwrite(f'{outputPath}{chapter}_long.png', long_image)
