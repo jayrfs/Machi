d9c1e2eb1638798dbb6604615759f8d7489b445a
unnatmalkoti <unnatmalkoti@gmail.com>
2021-08-13 18:54:35 +0530

    first

---
+++ main.py
@@ -0,0 +1,51 @@
+import cv2
+import imagehash
+import numpy as np
+from skimage.measure import compare_ssim as ssim 
+def CaptureFrames(num):
+  vidcap = cv2.VideoCapture(f'{num}.mp4')
+  success,image = vidcap.read()
+  count = 0
+  last_img = None
+
+
+  while success:
+    success,image = vidcap.read()
+    croped = image[78:994,630:1347]
+
+    cv2.imshow('video', croped)
+    if cv2.waitKey(1) == 27:
+      exit(0)
+    smol_size = tuple(map(lambda x: int(x*0.05) , croped.shape))
+    smol = cv2.resize(croped, (smol_size[0], smol_size[1]), interpolation = cv2.INTER_AREA)
+    smol_gray = cv2.cvtColor(smol,cv2.COLOR_BGR2GRAY)
+    print('Read a new frame: ', success)
+    if last_img is not None:
+      # breakpoint()
+      # this_hash = imagehash.average_hash(croped)
+      # last_hash = imagehash.average_hash(last_img)
+      
+      
+      s = ssim(smol_gray,last_img) 
+      # difference =  cv2.subtract(croped, last_img)
+      # result = not(np.bitwise_xor(croped,last_img).any())
+      # result = not np.any(difference)
+      # cv2.imshow('video', difference)
+      # breakpoint()
+      # print(s)
+      print(s)
+      if s > 0.9 :
+        continue
+    last_img = smol_gray
+    cv2.imwrite("frames/%d_frames/%d.jpg" %( num ,count), croped)     # save frame as JPEG file      
+    count += 1
+
+# breakpoint()
+# CaptureFrames(71)
+# CaptureFrames(73)
+# CaptureFrames(74)
+# CaptureFrames(75)
+# CaptureFrames(76)
+# CaptureFrames(77)
+# CaptureFrames(78)
+CaptureFrames(80)
---
+++ template.py
@@ -0,0 +1,32 @@
+import cv2
+import numpy as np
+method = cv2.TM_SQDIFF_NORMED
+chapter = 75
+prev_image = long_image = cv2.imread('frames/%d_frames/7.jpg' % chapter)
+
+count = 0
+for img_num in range(7,474):
+    #695
+    new_image = cv2.imread(f'frames/{chapter}_frames/{img_num}.jpg')
+    prev_image_cropped = prev_image[-500:-1,:]
+    # cv2.imshow('output', prev_image_cropped)
+    # cv2.waitKey(0)
+
+    result = cv2.matchTemplate(prev_image_cropped, new_image, method)
+
+    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+
+    MPx,MPy = mnLoc
+    trows,tcols = prev_image_cropped.shape[:2]
+
+    # cv2.rectangle(new_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+    # cv2.imshow('output',new_image)
+    long_image = np.concatenate((long_image, new_image[MPy + trows + 1:,:]), axis=0)
+    # cv2.imshow('long img', long_image)
+    # cv2.waitKey(0)
+
+    prev_image = new_image
+    count+=1
+    print(count)
+
+cv2.imwrite(f'{chapter}_long.png', long_image)
