7548281519a65bce1061e0546e2b7edc3455f7cc
jayrfs <rfsjay@gmail.com>
2021-08-14 14:44:53 +0530

    🎨 improve: converted code to callable functions

--- Frames-Stitcher.py
+++ Frames-Stitcher.py
@@ -14,49 +14,59 @@ inputPath = ".//input//"
 outputPath = ".//output//"
 
 #get chapter
-chapter = int(input(f"Choose chapter to stitch: {os.listdir(inputPath)} :"))
-prev_image = long_image = cv2.imread(f"{inputPath}{chapter}_frames/0.jpg")
-numberFiles = os.listdir(f"{inputPath}{chapter}_frames/")
+#chapter = int(input(f"Choose chapter to stitch: {os.listdir(inputPath)} :"))
+
+
+def stitchy_code(chapter_number,buffer_size='500'):
+
+    numberFiles = os.listdir(f"{inputPath}{chapter_number}_frames/")
+    prev_image = long_image = cv2.imread(f"{inputPath}{chapter_number}_frames/0.jpg")
+    count = 0
+    
+    print(f"{len(numberFiles)} images detected in chapter {chapter_number}")
+    for img_num in range(len(numberFiles)):
+        new_image = cv2.imread(f'{inputPath}{chapter_number}_frames/{img_num}.jpg')
+        prev_image_cropped = prev_image[-100:-1,:]
+        # cv2.imshow('output', prev_image_cropped)
+        # cv2.waitKey(0)
+
+        result = cv2.matchTemplate(prev_image_cropped, new_image, method)
+
+        mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+
+        MPx,MPy = mnLoc
+        trows,tcols = prev_image_cropped.shape[:2]
+
+        # cv2.rectangle(new_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+        # cv2.imshow('output',new_image)
+        long_image = np.concatenate((long_image, new_image[MPy + trows + 1:,:]), axis=0)
+        # cv2.imshow('long img', long_image)
+        # cv2.waitKey(0)
+
+        prev_image = new_image
+        count+=1
+        print(count,end=" ")
+
+    #assign old filename
+    filename = f"{outputPath}{chapter_number}_stitched.png"
+        
+    #call rename function and write
+    rename_old_files(filename)
+    cv2.imwrite(filename, long_image)
+
+    return
 
 #
-print(f"{len(numberFiles)} images detected in chapter {chapter}")
-
-count = 0
-for img_num in range(len(numberFiles)):
-    new_image = cv2.imread(f'{inputPath}{chapter}_frames/{img_num}.jpg')
-    prev_image_cropped = prev_image[-500:-1,:]
-    # cv2.imshow('output', prev_image_cropped)
-    # cv2.waitKey(0)
-
-    result = cv2.matchTemplate(prev_image_cropped, new_image, method)
-
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-
-    MPx,MPy = mnLoc
-    trows,tcols = prev_image_cropped.shape[:2]
-
-    # cv2.rectangle(new_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
-    # cv2.imshow('output',new_image)
-    long_image = np.concatenate((long_image, new_image[MPy + trows + 1:,:]), axis=0)
-    # cv2.imshow('long img', long_image)
-    # cv2.waitKey(0)
-
-    prev_image = new_image
-    count+=1
-    print(count,end=" ")
-
-#safeguard to avoid overwriting files
-filename = f"{outputPath}{chapter}_stitched.png"
-
 def rename_old_files(filename_old):
     file_duplicate = 0
     number_files_output = os.listdir(outputPath)
     for file in number_files_output:
-        if filename[:-4] in filename_old:
+        if filename_old[:-4] in filename_old:
             file_duplicate += 1
     if file_duplicate>0:
-        os.rename(filename,f"{filename[:-4]}_old({file_duplicate}).png")
+        os.rename(filename_old,f"{filename_old[:-4]}_old({file_duplicate}).png")
     return
 
-rename_old_files(filename)
-cv2.imwrite(filename, long_image)
+
+#safeguard to avoid overwriting files
+stitchy_code(74,500)
