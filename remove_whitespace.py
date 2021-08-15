af893d7b8627bf86db2c58f0d88411d7cc3150e5
jayrfs <rfsjay@gmail.com>
2021-08-15 23:19:53 +0530

    ✨ Add: whitespace remover

--- remove_whitespace.py
+++ remove_whitespace.py
@@ -2,6 +2,9 @@ import cv2
 import numpy as np
 method = cv2.TM_SQDIFF_NORMED
 
+#template
+whitespace_template = cv2.imread(f'.//resources//whitespace_template.jpg')
+
 #final_image
 
 def remove_whitespace(image):
@@ -16,12 +19,14 @@ def remove_whitespace(image):
     for h_line in range(image_height):
         crop_image = image[h_line:h_line+1, 0:image_width]
         print(h_line)
-        long_long_man = cv2.vconcat([long_long_man, crop_image])
+        is_whitespace = np.array_equal(crop_image,whitespace_template)
+        if is_whitespace==False:
+            long_long_man = cv2.vconcat([long_long_man, crop_image])
 
-    cv2.imwrite(f".//ignore//dump//output.jpg", long_long_man)
+    '''cv2.imwrite(f".//ignore//dump//output.jpg", long_long_man)
     print(f"image = {image.shape}")
     print(f"final_image = {final_image.shape}")
-    #np.savetxt("foo.csv", image, delimiter=",")
-    return
+    '''#np.savetxt("foo.csv", image, delimiter=",")
+    return(long_long_man)
 
 remove_whitespace(cv2.imread(f'.//input//74_frames//0.jpg'))
