a03f7dd4aac526fe5d19dcd09e55b7ba3991bc3d
jayrfs <rfsjay@gmail.com>
2021-08-17 15:03:30 +0530

    🔧 change: default stitcher values

---
+++ loop.py
@@ -0,0 +1,11 @@
+#from frames_stitcher import frames_stitcher
+import os
+
+#add paths
+inputPath = ".//input//"
+outputPath = ".//output//"
+extension = ".png"
+
+#get chapter
+list_of_files = os.listdir(inputPath)
+print(list_of_files)
