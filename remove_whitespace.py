bd2aa5e9354fd068a86007c24fe45c8f47acc914
jayrfs <rfsjay@gmail.com>
2021-08-17 14:39:59 +0530

    🐛 fix: horizontal width for scanner

--- remove_whitespace.py
+++ remove_whitespace.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 method = cv2.TM_SQDIFF_NORMED
 
-def remove_whitespace(image, tolerance=100, scanner_width=6969):
+def remove_whitespace(image, tolerance=100, scanner_width=100):
     
     #create placeholder image
     final_image = np.zeros((image.shape[0],image.shape[1],image.shape[2]))
@@ -11,12 +11,14 @@ def remove_whitespace(image, tolerance=100, scanner_width=6969):
     image_color = image.shape[2]
     long_long_man = image[0:1, 0:image_width]
 
-    if scanner_width == 6969:
-        scanner_width = image_width
+    '''if scanner_width == 6969:
+        scanner_width = image_width'''
 
     scanner_center = image_width//2
-    scanner_h_start = scanner_center - scanner_width
-    scanner_h_end = scanner_center + scanner_width
+    scanner_h_start = scanner_center - scanner_width//2
+    scanner_h_end = scanner_center + scanner_width//2
+
+    #print(f"scanner center = {scanner_center}\nscanner h start = {scanner_h_start}\nscanner h end = {scanner_h_end}")
 
     #read template
     whitespace_template = cv2.imread(f'.//resources//whitespace_template.jpg')
@@ -34,9 +36,11 @@ def remove_whitespace(image, tolerance=100, scanner_width=6969):
     print(f"\nimage height = {image_height}")
 
     #iterate over every line of the stitched image
-    for h_line in range(0,image_height, tolerance):
-        crop_image = image[h_line:h_line+tolerance, scanner_h_start:scanner_h_end]
-        is_whitespace = np.array_equal(crop_image,whitespace_template)
+    for h_line in range(0, image_height, tolerance):
+        crop_image = image[h_line:h_line+tolerance, 0:image_width]
+        test_image = image[h_line:h_line+tolerance, scanner_h_start:scanner_h_end]
+        #cv2.imwrite(f".//ignore/dele//test{h_line}.png", test_image)
+        is_whitespace = np.array_equal(test_image,whitespace_template)
         if is_whitespace == False:
             long_long_man = cv2.vconcat([long_long_man, crop_image])
         #simple check to print progress
@@ -57,4 +61,5 @@ def remove_whitespace(image, tolerance=100, scanner_width=6969):
     #cv2.imwrite(f".//trim.png", long_long_man)
     return(long_long_man)
 
-#remove_whitespace(cv2.imread(f'.//output//74_stitched.png'))
+image=remove_whitespace(cv2.imread(f'.//output//74_stitched.png'), 100, 500)
+cv2.imwrite(f".//output//74_stitched_trim.png", image)
