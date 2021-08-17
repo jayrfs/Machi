fe7f8d9e0709c7a0a60a306557b5aca7dc347eca
jayrfs <rfsjay@gmail.com>
2021-08-17 17:10:34 +0530

    ✨ Add: time tracker

--- loop.py
+++ loop.py
@@ -1,6 +1,10 @@
 #from frames_stitcher import frames_stitcher
 import os
+import time
 from scripts.frames_stitcher import stitchy_code
+
+#log start time
+start_time = time.perf_counter()
 
 #add paths
 inputPath = ".//input//"
@@ -10,16 +14,17 @@ extension = ".png"
 #get chapter
 list_of_files = os.listdir(inputPath)
 print("\n\t\tMachi Image-Stitcher")
-print("\nDetected files in .\input:\n")
+print("\nDetected folders in .\input:\n")
 print("\t[+] - Input")
 print("\t | ")
 for files in list_of_files:
     print(f"\t └ {files}")
-chapter = (input(f"\nChoose chapter to stitch or press enter to stitch all:"))
-print("\n")
+chapter = (input(f"\nChoose a folder to stitch or press enter to stitch all: "))
 
 if chapter != "":
     stitchy_code(chapter)
 else:
     for files in list_of_files:
         stitchy_code(int(files[:2]))
+
+print(f"\nProcess completed in {time.perf_counter()-start_time} seconds.")
