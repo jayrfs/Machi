839d151025c07a2da53cbe73dcabb09b7547c3e2
jayrfs <rfsjay@gmail.com>
2021-08-18 18:09:01 +0530

    🚧 add: smart splitter wip

---
+++ scripts/smart_splitter.py
@@ -0,0 +1,26 @@
+import cv2, os
+import numpy as np
+
+def smart_splitter(image, name="image", parts=5):
+
+    #var
+    image_height = image.shape[0]
+    image_width = image.shape[1]
+    image_color = image.shape[2]
+    part_height = image_height//parts
+    cropped_images = []
+
+    print(f"image_height = {image.shape[0]}")
+    print(f"image_width = {image.shape[1]}")
+    print(f"image_color = {image.shape[2]}")
+    print(f"part_height = {image_height//parts}")
+
+    for index, image_part in enumerate(range(0, image_height, part_height)):
+        print(index)
+        cropped_images.append(image[image_part:image_part+part_height,:])
+        if cropped_images[index].shape[0] == part_height:
+            cv2.imwrite(f"{str(name[:-4])}_{index}.png", cropped_images[index])
+    return(cropped_images)
+
+#frames_splitter(cv2.imread("Untitled.png"),100)
+smart_splitter(cv2.imread(".//input//71_stitched.png"),"test",10)
