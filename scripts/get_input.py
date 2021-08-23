bfaff525b7ee4ac84e09db5d0272c4a6fb615117
jayrfs <rfsjay@gmail.com>
2021-08-23 12:38:08 +0530

    🐛 update: filename in get_input

--- scripts/get_input.py
+++ scripts/get_input.py
@@ -1,11 +1,14 @@
-import os, cv2
+import os, cv2, shutil
+from pathlib import Path
 
 #iterate over input path and add objects to new list
 input_images=[]
+outputPath = ".//output//"
 
 def get_input(input_path):
     for element in input_path:
-        input_images.append(cv2.imread(element))
-    print(len(input_images))
-    return(input_images)
+        print(element)
+        filename = Path(element).name
+        shutil.copy(element, f"{outputPath}{filename}")
+    return()
         
