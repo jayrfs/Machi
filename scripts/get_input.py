9540b1a1c9bcff270b20492c57aaf031c6bdef7b
jayrfs <rfsjay@gmail.com>
2021-08-23 11:54:57 +0530

    🍻 add: memory leak

--- scripts/get_input.py
+++ scripts/get_input.py
@@ -1,29 +1,11 @@
-#from frames_stitcher import frames_stitcher
 import os, cv2
-import time
 
-#add paths
-inputPath = ".//input//"
-outputPath = ".//output//"
-extension = ".png"
+#iterate over input path and add objects to new list
+input_images=[]
 
-if chapter != "":
-    try:
-        photo = cv2.imread(f"{inputPath}{list_of_files[int(chapter)]}")
-        split_images = smart_splitter(photo, list_of_files[int(chapter)])
-        write_output(split_images[0],split_images[1])
-    except Exception as e:
-        print(e)
-        pass
-else:
-    for files in list_of_files:
-        try:
-            photo = cv2.imread(f"{inputPath}{files}")
-            split_images = smart_splitter(photo, files)
-            write_output(split_images[0],split_images[1])
-        except Exception as e:
-            print(e)
-            pass
-
-print(f"\nProcess completed in {time.perf_counter()-start_time} seconds.")
-input('\nPress ENTER to exit')
+def get_input(input_path):
+    for element in input_path:
+        input_images.append(cv2.imread(element))
+    print(len(input_images))
+    return(input_images)
+        
